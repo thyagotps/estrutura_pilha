fbf8bd06ed11e17779a956d1440c151e1e4c9118
thyagotps <31422423+thyagotps@users.noreply.github.com>
2017-08-28 20:41:05 -0300

    Código Fonte
    Código Fonte

---
+++ Module1.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim pilha As New Pilha()
+        Dim opcao As Integer
+
+        Do
+            Console.WriteLine("")
+            Console.WriteLine("")
+            Console.WriteLine("Estrutura de dados Pilha")
+            Console.WriteLine("Opções:")
+            Console.WriteLine("1 - Inserir")
+            Console.WriteLine("2 - Excluir")
+            Console.WriteLine("3 - Imprimir")
+            Console.WriteLine("4 - Menor")
+            Console.WriteLine("0 - Sair")
+            Console.WriteLine("Digite a opção: ")
+            opcao = Console.ReadLine()
+
+            If (opcao = 1) Then
+                Console.WriteLine("")
+                Console.WriteLine("Digite um número: ")
+                pilha.numero = Console.ReadLine()
+                pilha.Push()
+            End If
+
+            If (opcao = 2) Then
+                pilha.pop()
+            End If
+
+            If (opcao = 3) Then
+                pilha.Imprimir()
+            End If
+
+            If (opcao = 4) Then
+                pilha.Menor()
+            End If
+
+        Loop While opcao > 0
+    End Sub
+
+
+
+End Module
---
+++ Pilha.vb
@@ -0,0 +1,52 @@
+﻿Public Class Pilha
+
+    Public pilha() As Integer = {0, 0, 0, 0, 0}
+    Public posicao As Integer
+    Public numero As Integer
+
+    Public Sub New()
+        posicao = -1
+        numero = 0
+    End Sub
+
+    Public Sub Push()
+        If (posicao < 4) Then
+            posicao += 1
+            pilha(posicao) = numero
+            Console.WriteLine("Inserido !")
+        Else
+            Console.WriteLine("Fora da faixa !")
+        End If
+    End Sub
+
+    Public Sub pop()
+        If (posicao >= 0 And posicao <= 4) Then
+            pilha(posicao) = 0
+            posicao -= 1
+            Console.WriteLine("Excluído !")
+        Else
+            Console.WriteLine("Fora da faixa !")
+        End If
+    End Sub
+
+    Public Sub Imprimir()
+        Console.WriteLine("Impressão:")
+        For i As Integer = 0 To 4
+            Console.WriteLine(pilha(i))
+        Next
+    End Sub
+
+    Public Sub Menor()
+        Dim temp As Integer
+        For i As Integer = 0 To 4
+            temp = pilha(i)
+            For y As Integer = 0 To 4
+                If (temp > pilha(y)) Then
+                    temp = pilha(y)
+                End If
+            Next
+        Next
+        Console.WriteLine("Menor: " & temp)
+    End Sub
+
+End Class
